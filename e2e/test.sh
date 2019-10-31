12ce8ac9fe31de5bf7e0619df81af05a30eecd93
Micah Huber <micah@fastmail.us>
2019-10-31 09:44:02 -0400

    limit sleep command with until

--- e2e/test.sh
+++ e2e/test.sh
@@ -43,5 +43,13 @@ rbacBindings:
       - clusterRole: test-rbac-manager
 EOF
 
-sleep 10
+# wait up to 2 minutes for rbac-manager to create the binding
+counter=0
+until kubectl get clusterrolebinding/rbac-manager-definition-admins-test-rbac-manager; do
+  let "counter=counter+1"
+  sleep 10
+  if [ $counter -gt 11 ]; then
+    break
+  fi
+done
 kubectl auth can-i create deployments --as=system:serviceaccount:rbac-manager:test-rbac-manager
