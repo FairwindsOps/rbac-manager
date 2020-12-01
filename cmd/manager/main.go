2db87672db24cb8efc8a45cbd9c13ed88702dfd9
Andrew Suderman <andrew@sudermanjr.com>
2020-12-01 13:04:15 -0700

    logging

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -62,7 +62,7 @@ func main() {
 	logrus.Debug("Setting up client for manager")
 	cfg, err := config.GetConfig()
 	if err != nil {
-		logrus.Error(err, "unable to set up client config")
+		logrus.Error(err, ": unable to set up client config")
 		os.Exit(1)
 	}
 
@@ -70,7 +70,7 @@ func main() {
 	logrus.Debug("Setting up manager")
 	mgr, err := manager.New(cfg, manager.Options{})
 	if err != nil {
-		logrus.Error(err, "unable to set up overall controller manager")
+		logrus.Error(err, ": unable to set up overall controller manager")
 		os.Exit(1)
 	}
 
@@ -79,14 +79,14 @@ func main() {
 	// Setup Scheme for all resources
 	logrus.Debug("Setting up scheme")
 	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
-		logrus.Error(err, "unable add APIs to scheme")
+		logrus.Error(err, ": unable add APIs to scheme")
 		os.Exit(1)
 	}
 
 	// Setup all Controllers
 	logrus.Debug("Setting up controller")
 	if err := controller.Add(mgr); err != nil {
-		logrus.Error(err, "unable to register controller to the manager")
+		logrus.Error(err, ": unable to register controller to the manager")
 		os.Exit(1)
 	}
 
@@ -107,7 +107,7 @@ func main() {
 	// Start the Cmd
 	logrus.Info("Watching RBAC Definitions")
 	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
-		logrus.Error(err, "unable to run the manager")
+		logrus.Error(err, ": unable to run the manager")
 		os.Exit(1)
 	}
 }
