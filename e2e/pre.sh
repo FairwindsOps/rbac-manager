3afa8224ed4d76e8cdd780d1663e0c9d80ce0819
Andrew Suderman <andrew@sudermanjr.com>
2020-12-02 11:27:51 -0700

    split yaml out

--- e2e/pre.sh
+++ e2e/pre.sh
@@ -12,7 +12,7 @@ else
     echo "CI_SHA1: $CI_SHA1"
 fi
 
-yq w -d5 -i deploy/all.yaml 'spec.template.spec.containers[0].image' "quay.io/reactiveops/rbac-manager:dev-$CI_SHA1"
-cat deploy/all.yaml
+yq w -d5 -i deploy/2_deployment.yaml 'spec.template.spec.containers[0].image' "quay.io/reactiveops/rbac-manager:dev-$CI_SHA1"
+cat deploy/2_deployment.yaml
 
 docker cp deploy e2e-command-runner:/
