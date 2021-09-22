1f928de145ac4a56ff993778ab4e6babbd14a8e9
Luke Reed <luke@fairwinds.com>
2021-09-22 09:55:18 -0400

    Update documentation from template (#250)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -42,7 +42,7 @@ const baseConfig = {
   ],
   themeConfig: {
     docsRepo: "",
-    docsDir: 'docs-md',
+    docsDir: 'docs',
     editLinks: true,
     editLinkText: "Help us improve this page",
     logo: '/img/fairwinds-logo.svg',
@@ -55,7 +55,7 @@ const baseConfig = {
   plugins: {
     'vuepress-plugin-clean-urls': {
       normalSuffix: '/',
-      notFoundPath: '/error.html',
+      notFoundPath: '/404.html',
     },
     'check-md': {},
   },
