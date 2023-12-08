d990ae9c9ed802090a523e6a5a547e2951f5775b
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-12-09 00:34:34 +0100

    test: add chainsaw tests
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- e2e/test.sh
+++ e2e/test.sh
@@ -20,7 +20,21 @@ printf "\n\n"
 kubectl apply -f deploy/
 kubectl -n rbac-manager wait deployment/rbac-manager --timeout=120s --for condition=available
 
-bash "$BASE_DIR/rbacdefinition/run.sh"
+printf "\n\n"
+echo "********************************************************************"
+echo "** Install and run Chainsaw **"
+echo "********************************************************************"
+printf "\n\n"
+
+cd "$BASE_DIR/chainsaw"
+
+curl -sL https://github.com/kyverno/chainsaw/releases/download/v0.1.0/chainsaw_linux_amd64.tar.gz -o linux_amd64.tar.gz
+tar -xvf linux_amd64.tar.gz chainsaw
+rm linux_amd64.tar.gz
+chmod +x chainsaw
+
+./chainsaw test
+
 if [ $? -ne 0 ]; then
   exit 1
 fi
